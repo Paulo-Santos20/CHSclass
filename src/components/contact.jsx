85379acff063fc0ab60e11dd1b5cb547c2842c95
Paulo-Santos20 <paulo_santos20@outlook.com.br>
2023-09-03 13:24:12 -0300

    test: performance contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,47 +1,41 @@
 import emailjs from "@emailjs/browser";
 import contactImg from "../assets/img/contact-img.svg";
-import {Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import React, { useRef, useState, useEffect } from "react";
-
-
-// npm i @emailjs/browser
 
 const Contact = () => {
   const form = useRef();
-  const [emailStatus, setEmailStatus] = useState(""); // New state to track email status
+  const [emailStatus, setEmailStatus] = useState("");
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-
-  const sendEmail = (e) => {
+  const sendEmail = async (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
+    try {
+      const result = await emailjs.sendForm(
         "service_smgmwvg",
         "template_fxcj0bl",
         form.current,
         "VN7l4JdeSMDzGexk8"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          console.log("Mensagem enviada");
-          setEmailStatus("success");
-          setShowSuccessMessage(true);
-        },
-        (error) => {
-          console.log(error.text);
-          setEmailStatus("error");
-          setShowSuccessMessage(false);
-        }
       );
+      
+      console.log(result.text);
+      setEmailStatus("success");
+      setShowSuccessMessage(true);
+    } catch (error) {
+      console.log(error.text);
+      setEmailStatus("error");
+      setShowSuccessMessage(false);
+    }
   };
 
   useEffect(() => {
     if (showSuccessMessage) {
-      setTimeout(() => {
+      const timer = setTimeout(() => {
         setShowSuccessMessage(false);
-      }, 3000); // Hide after 3 seconds
+      }, 3000);
+
+      return () => clearTimeout(timer);
     }
   }, [showSuccessMessage]);
 
@@ -65,18 +59,18 @@ const Contact = () => {
         </Row>
 
         <Row>
-          <label></label>          
-            <textarea rows="6" name="message" placeholder="Mensagem" />          
+          <label></label>
+          <textarea rows="6" name="message" placeholder="Mensagem" />
         </Row>
-        
+
         <input type="submit" value="Enviar" />
       </form>
       {showSuccessMessage && (
         <div className="overlay">
           <p className="success-message">Email enviado com sucesso!</p>
         </div>
-        )}
-        </div>
+      )}
+    </div>
   );
 };
 
